3d12dfe6be8ff5b09472543ebcd065bf0a9748a9
RakovBogdan <rakovbogdan@gmail.com>
2017-08-02 16:24:35 +0300

    Prototype implementation with factory

---
+++ src/main/java/org/bohdanrakov/designpatterns/creational/prototype/HumanFactory.java
@@ -0,0 +1,18 @@
+package org.bohdanrakov.designpatterns.creational.prototype;
+
+public class HumanFactory {
+
+    Human human;
+
+    public HumanFactory(Human human) {
+        setPrototype(human);
+    }
+
+    public void setPrototype(Human human) {
+        this.human = human;
+    }
+
+    Human makeCopy() {
+        return (Human)human.copy();
+    }
+}
--- src/main/java/org/bohdanrakov/designpatterns/creational/prototype/Main.java
+++ src/main/java/org/bohdanrakov/designpatterns/creational/prototype/Main.java
@@ -3,10 +3,16 @@ package org.bohdanrakov.designpatterns.creational.prototype;
 public class Main {
 
     public static void main(String[] args) {
-        Human original = new Human(18, "Bohdan");
+        Human original = new Human(22, "Bohdan");
         System.out.println(original);
 
         Human copy = (Human)original.copy();
         System.out.println(copy);
+
+        HumanFactory factory = new HumanFactory(copy);
+        System.out.println(factory.makeCopy());
+
+        factory.setPrototype(new Human(23, "Slavik"));
+        System.out.println(factory.makeCopy());
     }
 }
